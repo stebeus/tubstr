ded589cb09b1066cf75c73fe4d49b65c3d7d9093
stebeus <stebeus@mailfence.com>
2025-11-01 18:03:45 -0300

    Import stylelint-twwo-dash-bem config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard"],
+  extends: ["stylelint-config-standard", "stylelint-config-two-dash-bem"],
   plugins: ["stylelint-order"],
   rules: {
     // Color
